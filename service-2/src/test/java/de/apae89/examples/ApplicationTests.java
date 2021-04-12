c598dd1fce97acd65e9439e14b57d7bfc246e39d
Andreas Pätzold <andreas.paetzold@msg-david.de>
2021-04-12 11:57:31 +0200

    Change package name

---
+++ service-2/src/test/java/de/apae89/examples/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.apae89.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
